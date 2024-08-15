12af3d68923aa32202df0a9f9fc95cfd1df6b463
ValdineideSchunk <valdineideschunk@gmail.com>
2024-08-15 13:24:44 -0300

    atualização menu lateral

--- menu_lateral/js/script.js
+++ menu_lateral/js/script.js
@@ -1,3 +1,18 @@
 document.getElementById('open_btn').addEventListener('click', function () {
-    document.getElementById('sidebar').classList.toggle('open-sidebar');
+    const sidebar = document.getElementById('sidebar');
+    const descriptions = document.querySelectorAll('.item-description');
+    
+    if (sidebar.classList.contains('open-sidebar')) {
+        // Esconde os nomes e fecha o menu simultaneamente
+        sidebar.classList.remove('open-sidebar');
+        descriptions.forEach(description => {
+            description.style.width = '0px';
+        });
+    } else {
+        // Abre o menu e mostra os nomes simultaneamente
+        sidebar.classList.add('open-sidebar');
+        descriptions.forEach(description => {
+            description.style.width = '120px';
+        });
+    }
 });
